eed68dc82faebf8507665cfab099f07793436951
ayoube01 <aybuchi@gmail.com>
2018-03-03 19:24:35 +0100

    Add files via upload

---
+++ fonction_data_exemple.py
@@ -0,0 +1,8 @@
+def make_df(cols, ind):
+    """Quickly make a DataFrame"""
+    data = {c: [str(c) + str(i) for i in ind]
+            for c in cols}
+    return pd.DataFrame(data, ind)
+
+# example DataFrame
+make_df('ABC', range(3))
